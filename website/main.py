e15a9cc04c238aa1e7d8f56ace4a35b762bbbd4d
denavit <denavit@gmail.com>
2024-06-15 22:57:35 +0800

    Update output text

--- website/main.py
+++ website/main.py
@@ -52,6 +52,14 @@ def calculations():
                         main_text = output_text,
                         member_list = names())
 
+'''
+Text for header
+
+<p>As noted in AISC <i>Specification</i> Commentary Section E4, torsional buckling is a failure mode usually not considered in 
+the design of hot-rolled columns. This failure mode generally does not govern unless the section is manufactured from relatively 
+thin plates or a torsional unbraced length significantly larger than the <i>y-y</i> axis flexural unbraced length is present.</p>
+'''
+
 def generate_output_text(member,Fy,Eksi,Lcx,Lcy):
 
     text = ''
@@ -73,214 +81,162 @@ def generate_output_text(member,Fy,Eksi,Lcx,Lcy):
     htw = wide_flange_database[member]['h/tw']
 
     text += f'''
-    <div class="col-5">
-        <p>From AISC <i>Manual</i> Table 1-1, the geometric properties for a {member} are as follows:</p>
-        <ul>
-          <li>$A_{{g}} = {{{'%.2f'%A}}}\\text{{ in.}}^{{2}}$</li> 
-          <li>$r_{{x}} = {{{'%.2f'%rx}}}\\text{{ in.}}$</li>
-          <li>$r_{{y}} = {{{'%.2f'%ry}}}\\text{{ in.}}$</li>
-          <li>$\\frac{{b_{{f}}}}{{2t_{{f}}}} = {{{'%.2f'%bf2tf}}}$</li>
-          <li>$\\frac{{h}}{{t_{{w}}}} = {{{'%.2f'%htw}}}$</li>
-        </ul>
-    </div>'''
+      <p>From AISC <i>Manual</i> Table 1-1, the geometric properties for a {member} are as follows:</p>
+      <p>$A_{{g}} = {{{'%.2f'%A}}}\\text{{ in.}}^{{2}}$</p> 
+      <p>$r_{{x}} = {{{'%.2f'%rx}}}\\text{{ in.}}$</p>
+      <p>$r_{{y}} = {{{'%.2f'%ry}}}\\text{{ in.}}$</p>
+      <p>$\\dfrac{{b_{{f}}}}{{2t_{{f}}}} = {{{'%.2f'%bf2tf}}}$</p>
+      <p>$\\dfrac{{h}}{{t_{{w}}}} = {{{'%.2f'%htw}}}$</p>
+    '''
 
     # Slenderness Check
     lambdar_flange = 0.56 * sqrt(Eksi / Fy)
     lambdar_web = 1.49 * sqrt(Eksi / Fy)
 
     text += f'''
-    <div class="col-5">  
       <h1>Slenderness Check</h1>
       <p>The width-to-thickness ratio of the flanges of the {member} is:</p>
-      <ul>
-        <li>$\\frac{{b_{{f}}}}{{2t_{{f}}}} = {{{'%.2f'%bf2tf}}}$</li>
-      </ul>
+      <p>$\\dfrac{{b_{{f}}}}{{2t_{{f}}}} = {{{'%.2f'%bf2tf}}}$</p>
       <p>From AISC <i>Specification</i> Table B4.1a, Case 1, the limiting width-to-thickness ratio of the flanges is:</p>
-      <ul>
-        <li>'''
+      <p>'''
         
-    text += r'$\begin{aligned}\lambda_{r} &= 0.56\sqrt{\frac{E}{F_{y}}} \\ &= 0.56\sqrt{\frac{' + sigfigstr(Eksi) + r'\text{ ksi}}{' + \
-            sigfigstr(Fy) + r'\text{ ksi}}} \\ &= ' + sigfigstr(lambdar_flange) + r'\end{aligned}$'
-    
-    text += '''</li>
-        <li>'''    
-    
+    text += r'$\begin{aligned}\lambda_{r} &= 0.56\sqrt{\dfrac{E}{F_{y}}} \\ &= 0.56\sqrt{\dfrac{' + sigfigstr(Eksi) + r'\text{ ksi}}{' + \
+            sigfigstr(Fy) + r'\text{ ksi}}} \\ &= ' + sigfigstr(lambdar_flange)
+
     if bf2tf > lambdar_flange:
-        text += r'$\frac{b_f}{2t_f} = ' + sigfigstr(bf2tf) + r' > \lambda_f =' + sigfigstr(lambdar_flange) + r'$; therefore the flanges are slender'
+        text += r' < ' + sigfigstr(bf2tf) + r'\end{aligned}$</p>'
+        text += '\n      <p>therefore the flanges are slender</p>'
     else:
-        text += r'$\frac{b_f}{2t_f} = ' + sigfigstr(bf2tf) + r' \leq \lambda_f =' + sigfigstr(lambdar_flange) + r'$; therefore the flanges are nonslender'
+        text += r' \geq ' + sigfigstr(bf2tf) + r'\end{aligned}$</p>' 
+        text += '\n      <p>therefore the flanges are nonslender</p>'
     
-    text += f'''</li>
-      </ul>
+    text += f'''
       <p>The width-to-thickness ratio of the web of the {member} is:</p>
-      <ul>
-        <li>$\\frac{{h}}{{t_{{w}}}} = {{{'%.2f'%htw}}}$</li>
-      </ul>
+      <p>$\\dfrac{{h}}{{t_{{w}}}} = {{{'%.2f'%htw}}}$</p>
       <p>From AISC <i>Specification</i> Table B4.1a, Case 5, the limiting width-to-thickness ratio of the web is:</p>
-      <ul>
-        <li>'''
+      <p>'''
         
-    text += r'$\begin{aligned}\lambda_{r} &= 1.49\sqrt{\frac{E}{F_{y}}} \\ &= 1.49\sqrt{\frac{' + sigfigstr(Eksi) + r'\text{ ksi}}{' + \
-            sigfigstr(Fy) + r'\text{ ksi}}} \\ &= ' + sigfigstr(lambdar_web) + r'\end{aligned}$'
-    
-    text += '''</li>
-        <li>'''    
-    
+    text += r'$\begin{aligned}\lambda_{r} &= 1.49\sqrt{\dfrac{E}{F_{y}}} \\ &= 1.49\sqrt{\dfrac{' + sigfigstr(Eksi) + r'\text{ ksi}}{' + \
+            sigfigstr(Fy) + r'\text{ ksi}}} \\ &= ' + sigfigstr(lambdar_web)
+        
     if htw > lambdar_web:
-        text += r'$\frac{h}{t_w} = ' + sigfigstr(htw) + r' > \lambda_f =' + sigfigstr(lambdar_web) + r'$; therefore the web is slender'       
+        text += r' < ' + sigfigstr(htw) + r'\end{aligned}$</p>'
+        + r'$; therefore the web is slender'      
+        text += '\n      <p>therefore the web is slender</p>\n'
+        
     else:
-        text += r'$\frac{h}{t_w} = ' + sigfigstr(htw) + r' \leq \lambda_f =' + sigfigstr(lambdar_web) + r'$; therefore the web is nonslender'       
-    
-    text += f'''</li>
-      </ul>'''
+        text += r' \geq ' + sigfigstr(htw) + r'\end{aligned}$</p>'
+        text += '\n      <p>therefore the web is nonslender</p>\n'
+
       
     if bf2tf > lambdar_flange:
         if htw > lambdar_web:
-            text += r'Because the web and flanges are slender, the strength must be computed using AISC <i>Specification</i> Section E6 and will not be shown here.'
-            text += '''  
-    </div>'''            
+            text += r'      <p>Because the web and flanges are slender, the strength must be computed using AISC <i>Specification</i> Section E6 and will not be shown here.</p>'
             return text
         else:
-            text += r'Because the flanges are slender, the strength must be computed using AISC <i>Specification</i> Section E6 and will not be shown here.'
-            text += '''  
-    </div>'''            
+            text += r'      <p>Because the flanges are slender, the strength must be computed using AISC <i>Specification</i> Section E6 and will not be shown here.</p>'
             return text
     else:
         if htw > lambdar_web:
-            text += r'Because the web is slender, the strength must be computed using AISC <i>Specification</i> Section E6 and will not be shown here.'
-            text += '''  
-    </div>'''            
+            text += r'      <p>Because the web is slender, the strength must be computed using AISC <i>Specification</i> Section E6 and will not be shown here.</p>'
             return text
         else:
-            text += r'Because the web and flanges are nonslender, the limit state of local buckling does not apply and the strength is computed according to AISC <i>Specification</i> Section E3.'
-            
-    text += '''  
-    </div>'''
+            text += r'      <p>Because the web and flanges are nonslender, the limit state of local buckling does not apply and the strength is computed according to AISC <i>Specification</i> Section E3.</p>'
 
 
     # Gets Critical Stresses Data
-    Lcxrx = Lcx / rx
-    Lcyry = Lcy / ry
-    Lcr = max(Lcxrx, Lcyry)
-    if Lcxrx < Lcyry:
-        control_sign = '<'
-        controlling_axis = 'minor axis controls'
-    else:
-        control_sign = '>'
-        controlling_axis = 'major axis controls'    
-
     text += f'''
-    <div class="col-5">  
-      <h1>Governing Slenderness Ratio</h1>
-      <ul>
-        <li>
-          $\\frac{{L_{{cx}}}}{{r_{{x}}}} = \\frac{{{'%.2f'%Lcx}\\text{{ in.}}}}
-          {{{'%.2f'%rx}\\text{{ in.}}}} = {{{'%.2f'%Lcxrx}}}\\text{{ governs}}$
-        </li>
-        <br/>
-        <li>
-          $\\frac{{L_{{cy}}}}{{r_{{y}}}} = \\frac{{{'%.2f'%Lcy}\\text{{ in.}}}}{{
-          {{{'%.2f'%ry}\\text{{ in.}}}}}}= {{{'%.2f'%Lcyry}}}$
-        </li>
-        <br/>
-        <li>
-          ${{{'%.2f'%Lcxrx}}} {control_sign} {{{'%.2f'%Lcyry}}}$; therefore, the
-          {controlling_axis}
-        </li>
-        <br/>
-        <li>
-          $\\frac{{L_{{c}}}}{{r}} = \\text{{max}}(\\frac{{L_{{cx}}}}{{r_{{x}}}},
-          \\frac{{L_{{cy}}}}{{r_{{y}}}}) = \\text{{max}}({{{'%.2f'%Lcxrx}}},
-          {{{'%.2f'%Lcyry}}}) = {{{'%.2f'%Lcr}}}$
-        </li>
-      </ul>      
+      <h1>Slenderness Ratio</h1>
+      <p>'''
+
+    text += r'$\dfrac{L_{cx}}{r_x} = \dfrac{' + sigfigstr(Lcx) + r'\text{ in.}}{' + \
+            sigfigstr(rx) + r'\text{ in.}} = ' + sigfigstr(Lcx/rx) + r'$'
+
+    text += f'''</p>
+      <p>'''
+
+    text += r'$\dfrac{L_{cy}}{r_y} = \dfrac{' + sigfigstr(Lcy) + r'\text{ in.}}{' + \
+            sigfigstr(ry) + r'\text{ in.}} = ' + sigfigstr(Lcy/ry) + r'$'
+
+    text += f'''</p>
+      <p>'''
       
-    </div>'''
-    
-    Fe = pi**2 * Eksi / Lcr**2
-    Fcr = 4.71 * sqrt(Eksi / Fy)
-    Fcr1 = 0.658**(Fy/Fe)*Fy
-    Fcr2 = 0.877 * Fe
-
-    if Lcr < Fcr:
-        buckling_sign = '<'
-        buckling_method_equ = f'''$F_{{cr}} = (0.658^{{\\frac{{F_{{y}}}}{{F_{{e}}}}}})F_{{y}} = (0.658^\\frac{{
-              {'%.2f'%Fy}\\text{{ ksi}}}}{{{'%.2f'%Fe}\\text{{ ksi}}}})({{{'%.2f'%Fy}\\text{{ ksi}}}}) = {{{'%.2f'%Fcr1}\\text{{ ksi}}}}$'''
-        Fcr3 = Fcr1
-    elif Lcr == Fcr:
-        buckling_sign = '='
-        buckling_method_equ = f'''$F_{{cr}} = (0.658^{{\\frac{{F_{{y}}}}{{F_{{e}}}}}})F_{{y}} = (0.658^\\frac{{
-              {'%.2f'%Fy}\\text{{ ksi}}}}{{{'%.2f'%Fe}\\text{{ ksi}}}})({{{'%.2f'%Fy}\\text{{ ksi}}}}) = {{{'%.2f'%Fcr1}\\text{{ ksi}}}}$'''
-        Fcr3 = Fcr1
+    if Lcx/rx > Lcy/ry:
+        text += r'Because $\dfrac{L_{cx}}{r_x} > \dfrac{L_{cy}}{r_y}$, major-axis buckling controls and $\dfrac{L_{c}}{r} =  ' + sigfigstr(Lcx/rx) + '$'
+        Lc_over_r = Lcx/rx
     else:
-        buckling_sign = '>'
-        buckling_method_equ = f'''$F_{{cr}} = 0.877Fe = 0.877({'%.2f'%Fe}) = {'%.2f'%Fcr2}\\text{{ ksi}}$'''
-        Fcr3 = Fcr2
+        text += r'Because $\dfrac{L_{cy}}{r_y} > \dfrac{L_{cx}}{r_x}$, minor-axis buckling controls and $\dfrac{L_{c}}{r} =  ' + sigfigstr(Lcy/ry) + '$'
+        Lc_over_r = Lcy/ry
 
-    # Gets Nominal Compressive Strength Data
-    Pn = Fcr3 * A
-    phiPn = 0.9 * Pn
-    omegaPn = Pn / 1.67    
     
     text += f'''
-    <div class="col-5">
-      <h1>Critical Stress</h1>
-      <p>The available critical stresses may be interpolated from AISC <i>Manual</i> Table 4-14 or calculated directly as follows.</p>
-      <p>Calculate the elastic critical buckling stress, $F_{{e}}$, according to AISC <i>Specification</i> Section E3. As noted in AISC
-         <i>Specification</i> Commentary Section E4, torsional buckling of symmetric shapes is a failure mode usually not considered in 
-         the design of hot-rolled columns. This failure mode generally does not govern unless the section is manufactured from relatively 
-         thin plates or a torsional unbraced length significantly larger than the <i>y-y</i> axis flexural unbraced length is present.</p>
-      <ul>
-        <li>
-          $F_{{e}} = \\frac{{\pi ^{{2}}E}}{{(\\frac{{L_{{c}}}}{{r}})^{{2}}}} = \\frac{{\pi
-          ^{{2}}( {{{'%.2f'%Eksi}}} )}}{{{'%.2f'%Lcr}}} ^{{2}} =
-          {{{'%.2f'%Fe}}} \\text{{ ksi}}$
-        </li>
-      </ul>
-      <p>Calculate the flexural buckling stress, $F_{{cr}}$.</p>
-      <ul>
-        <li>
-          $4.71\sqrt{{\\frac{{E}}{{F_{{y}}}}}} = 4.71\sqrt\\frac{{{'%.2f'%Eksi}}}
-          {{{'%.2f'%Fy}}} = {{{'%.2f'%Fcr}}}$
-        </li>
-      </ul>
-      <p>Because $\\frac{{L_{{c}}}}{{r}} = {{{'%.2f'%Lcr}}} {buckling_sign}
-        {{{'%.2f'%Fcr}}}$</p>
-      <ul>
-        {buckling_method_equ}
-      </ul>
-    </div>'''
-
-
-    text += f'''
-    <div class="col-5">
       <h1>Nominal Compressive Strength</h1>
-      <ul>
-        <li>
-          $P_{{n}}=F_{{cr}}A_{{g}} = ({'%.2f'%Fcr1}\\text{{
-          ksi}})({'%.2f'%A}\\text{{ in.}}^{{2}}) = {'%.2f'%Pn}\\text{{
-          kips}}$
-        </li>
-      </ul>
-    </div>'''
-          
-       
+
+      <p>The available critical stresses may be interpolated from AISC <i>Manual</i> Table 4-14 or calculated directly according to AISC <i>Specification</i> Section E3 as follows.</p>
+      <p>Calculate the elastic critical buckling stress, $F_{{e}}$</p>
+      <p>'''
+
+    Fe = pi**2 * Eksi / Lc_over_r**2
+    text += r'$\begin{aligned}F_e &= \dfrac{\pi^2 E}{\left(\dfrac{L_c}{r}\right)^2} \\' + \
+            r'&= \dfrac{\pi^2 (' + sigfigstr(Eksi) + r'\text{ ksi})}{(' + sigfigstr(Lc_over_r) + r')^2} \\' + \
+            r'&= ' + sigfigstr(Fe) + r'\text{ ksi} \end{aligned}$</p>' 
+
     text += f'''
-    <div class="col-5">  
-      <h1>Available Compressive Strength</h1>
-      <p>From AISC <i>Specification</i> Section E1, the available compressive strength is:</p>
-      <p><b>LRFD:</b></p>
-      <ul>
-        <li>$\\phi_{{c}} = 0.90$</li>
-        <li>$\\phi_{{c}}P_{{n}} = 0.90({'%.2f'%Pn}\\text{{ kips}}) = {'%.2f'%phiPn}\\text{{ kips}}$</li>
-      </ul>
-      <br />
-      <p><b>ASD:</b></p>
-      <ul>
-        <li>$\\Omega_{{c}} = 1.67$</li>
-        <li>$\\frac{{P_{{n}}}}{{\\Omega_{{c}}}} = \\frac{{{'%.2f'%Pn}\\text{{ kips}}}}{{1.67}} = {'%.2f'%omegaPn}\\text{{ kips}}$</li>
-      </ul>
-    </div>'''
+      <p>Calculate the nominal stress, $F_{{n}}$</p>
+      <p>'''
+
+    text += r'$4.71\sqrt{\dfrac{E}{F_y}} = 4.71\sqrt{\dfrac{' + sigfigstr(Eksi) + r'\text{ ksi}}{' + sigfigstr(Fy) + r'\text{ ksi}}} = ' + sigfigstr(4.71*sqrt(Eksi/Fy)) + r'$'
+    
+    text += f'''</p>
+      <p>'''
+    
+    if Lc_over_r <= 4.71*sqrt(Eksi/Fy):
+        text += r'Because $\dfrac{L_c}{r} = ' + sigfigstr(Lc_over_r) + r'\leq 4.71\sqrt{\dfrac{E}{F_y}} = ' + sigfigstr(4.71*sqrt(Eksi/Fy)) + r'$, inelastic buckling governs and $F_n$ is determined using AISC <i>Specification</i> Equation E3-2'
+
+        text += f'''</p>
+      <p>'''
+
+
+        Fn = 0.658**(Fy/Fe)*Fy
+
+        text += r'$\begin{aligned}F_n &= \left(0.658^{\dfrac{F_y}{F_e}}\right)F_y \\' + \
+                r'&= \left[0.658^\dfrac{' + sigfigstr(Fy) + r'\text{ ksi}}{' + sigfigstr(Fe) + r'\text{ ksi}}\right](' + sigfigstr(Fy) + r'\text{ ksi}) \\' + \
+                r'&=' + sigfigstr(Fn) + r'\text{ ksi} \end{aligned}$</p>'
+                
+    else:
+        text += r'Because $\dfrac{L_c}{r} = ' + sigfigstr(Lc_over_r) + r'> 4.71\sqrt{\dfrac{E}{F_y}} = ' + sigfigstr(4.71*sqrt(Eksi/Fy)) + r'$, elastic buckling governs and $F_n$ is determined using AISC <i>Specification</i> Equation E3-3'
+    
+        text += f'''</p>
+      <p>'''
+
+        Fn = 0.877 * Fe
+
+        text += r'$\begin{aligned}F_n &= 0.877 F_e \\' + \
+                r'&= 0.877 (' + sigfigstr(Fe) + r'\text{ ksi}) \\' + \
+                r'&=' + sigfigstr(Fn) + r'\text{ ksi} \end{aligned}$</p>'
+
+
+    text += f'''
+      <p>Calculate the nominal axial compressive strength, $P_{{n}}$, using  AISC <i>Specification</i> Equation E3-1.</p>
+      <p>'''
+    Pn = Fn * A
+
+    text += r'$P_n = F_n A_g = (' + sigfigstr(Fn) + r'\text{ ksi})(' + sigfigstr(A) + r'\text{ in.}^2) = ' + sigfigstr(Pn) + r'\text{ kips} $</p>'
+
+    phiPn = 0.9 * Pn
+    omegaPn = Pn / 1.67 
+    
+    text += f'''
+      <p>For LRFD:</p>
+      <p>'''
+
+    text += r'$\phi_c P_n = 0.90 (' + sigfigstr(Pn) + r'\text{ kips}) = ' + sigfigstr(0.9*Pn) + r'\text{ kips} $</p>'
+
+    text += f'''
+      <p>For ASD:</p>
+      <p>'''
+
+    text += r'$\dfrac{P_n}{\Omega_c} = \dfrac{(' + sigfigstr(Pn) + r'\text{ kips})}{1.67} = ' + sigfigstr(Pn/1.67) + r'\text{ kips} $</p>'
 
     return text
 
