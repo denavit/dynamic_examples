44b54f10d3e1b2bd74693fe7eebae457940f6e05
denavit <denavit@gmail.com>
2025-01-21 05:04:40 -0500

    Update FEDSTD_text.py

--- website/pageText/FEDSTD_text.py
+++ website/pageText/FEDSTD_text.py
@@ -108,15 +108,15 @@ def boltText_output(bolt, UTSs_str,UTSn_str,LE_str):
 
     text.newline('Thread Form Dimensions', tag='h1')
     text.newline(f'A {bolt} thread has a nominal size of ' + sigfigstr(boltObj.dbsc) + ' in. and ' + str(boltObj.n) + ' threads/in.', tag='p')
-    text.newline(r'$d = ' + sigfigstr(boltObj.dbsc) + r'\text{ in.}$',tag='p')
+    text.newline(r'$d_{bsc} = ' + sigfigstr(boltObj.dbsc) + r'\text{ in.}$',tag='p')
     text.newline(r'$n = ' + str(boltObj.n) + r'\text{ threads/in.}$',tag='p')
     
     text.newline(f'Based on the basic profile for UN screw threads shown in ASTM B1.1-2019 Figure 2, basic thread dimensions are:', tag='p')    
     text.newline(r'$p = \dfrac{1}{n} = \dfrac{1}{' + str(boltObj.n) + r'\text{ threads/in.}} = ' + sigfigstr(1/boltObj.n) + r'\text{ in.}$',tag='p')
     text.newline(r'$H = \dfrac{\sqrt{3}}{2n} = \dfrac{\sqrt{3}}{2(' + str(boltObj.n) + r'\text{ threads/in.})} = ' + sigfigstr(sqrt(3)/2/boltObj.n) + r'\text{ in.}$',tag='p')
     
-    text.newline(r'$D_{1,bsc} = d_{1,bsc} = ' + sigfigstr(boltObj.D1bsc) + r'$ in.', tag='p')
-    text.newline(r'$D_{2,bsc} = d_{2,bsc} = ' + sigfigstr(boltObj.D2bsc) + r'$ in.', tag='p')
+    text.newline(r'$D_{1,bsc} = d_{1,bsc} = ' + sigfigstr(boltObj.D1bsc) + r'\text{ in.}$', tag='p')
+    text.newline(r'$D_{2,bsc} = d_{2,bsc} = ' + sigfigstr(boltObj.D2bsc) + r'\text{ in.}$', tag='p')
 
     text.newline(f'From ASME B1.1-2019 Table 2A, the minimum dimensions of the external threads are:', tag='p')
     text.newline(r'$d_{min} = ' + sigfigstr(boltObj.dmin) + r'\text{ in.}$',tag='p')
@@ -126,22 +126,20 @@ def boltText_output(bolt, UTSs_str,UTSn_str,LE_str):
     text.newline(r'$D_{1,max} = ' + sigfigstr(boltObj.D1max) + r'\text{ in.}$',tag='p')
     text.newline(r'$D_{2,max} = ' + sigfigstr(boltObj.D2max) + r'\text{ in.}$',tag='p')
     
-    
+    # Tensile Strength
     text.newline(f'Tensile Strength', tag='h1')
-    text.newline(f'The ultimate tensile strength of a threaded fastener is directly proportional to the cross sectional area through the threaded region.', tag='p')
-    text.newline(f'FED-STD-H28/2B provides two equations to calculate the tensile stress area, both of which are geometrically identical',tag='p')
-    # text.newline(r'$A_{s,1a} = \pi\left(\dfrac{d_{2,bsc}}{2} - \dfrac{3H}{16}\right)^{2} = \pi\left(\left(\dfrac{1}{2}\right)\left(d_{2,bsc} - \dfrac{3H}{8}\right)\right)^{2} = \dfrac{\pi}{4}\left(d_{2,bsc} - \dfrac{3H}{8}\right)^{2}$', tag='p')
-    # text.newline(r'$A_{s,1b} = \dfrac{\pi}{4}\left(d_{bsc} - \dfrac{0.9743}{n}\right)^{2} = \dfrac{\\pi}{4}\left(d_{bsc} - \dfrac{9P\sqrt{3}}{16}\right)^{2} = \dfrac{\pi}{4}\left(d_{bsc} - \dfrac{9\sqrt{3}\left(\dfrac{2H}{\sqrt{3}}\right)}{16}\right)^{2} = \dfrac{\pi}{4}\left(d_{bsc} - \dfrac{9H}{8}\right)^{2}$', tag='p')
-    # text.newline(f'Figure 2 in ASME B1.1-2019 provides thread geometry to further prove the equality:', tag='p')
-    # text.newline(r'$d_{2,bsc} = d_{bsc} - 2\left(\dfrac{3H}{8}\right) = d_{bsc} - \dfrac{6H}{8}', tag='p')    
-    # text.newline(r'$A_{s, 1a} = \frac{\pi}{4}\left(d_{bsc} - \dfrac{6H}{8} - \dfrac{3H}{8}\right)^{2} = \dfrac{\pi}{4}\left(d_{bsc} - \dfrac{9H}{8}\right)^{2} = A_{s, 1b}$', tag='p')  
-    text.newline(r'For this example, equation $A_{s, 1a}$ will be used', tag='p')
-    text.newline(r'$A_{s, 1a} = \pi\left(\dfrac{d_{2,bsc}}{2}-\dfrac{3H}{16}\right)^{2}$', tag='p')   
-    text.newline(r'$= \pi\left(\dfrac{' + sigfigstr(boltObj.d2bsc) + r'\text{ in.}}{2} - \dfrac{3(' + sigfigstr(boltObj.H) + r'\text{ in.})}{16}\right)^{2}$', tag='p') 
-    text.newline(r'$= ' + sigfigstr(boltObj.As_FEDSTD_1a()) + r' \text{ in.}^{2}$', tag='p')
-    text.newline(f'The tensile strength of a threaded fastener is equal to:', tag='p')
-    text.newline(r'$P = \dfrac{F}{A} \therefore F = PA$', tag='p')    
-    text.newline(r'$F = UTSs\left(A_{s,1a}\right) = ' + str(boltObj.UTSs) + r'\text{ psi}\left(' + sigfigstr(boltObj.As_FEDSTD_1a()) + r'\text{ in.}^{2}\right) = ' + sigfigstr(boltObj.UTSs * boltObj.As_FEDSTD_1a()) + r'\text{ lbs}$', tag='p')
+    text.newline(f'The tensile strength of a fastener is equal to the ultimate tensile strength of the material times the tensile stress area.', tag='p')
+    text.newline(f'The two formulas for tensile stress area in FED-STD-H28/2B Table II.B.1 are geometrically identical (however, differences may result from rounding). For this example, formula (1a) is used.',tag='p')
+    
+    line = r'$\begin{aligned}A_{s} &= \pi\left(\dfrac{d_{2,bsc}}{2}-\dfrac{3H}{16}\right)^{2} \\'
+    line += r'&= \pi\left(\dfrac{' + sigfigstr(boltObj.d2bsc) + r'\text{ in.}}{2} - \dfrac{3(' + sigfigstr(boltObj.H) + r'\text{ in.})}{16}\right)^{2} \\'
+    line += r'&= ' + sigfigstr(boltObj.As_FEDSTD_1a()) + r' \text{ in.}^{2}\end{aligned}$'
+    text.newline(line, tag='p')   
+
+    text.newline(f'Calculate the tensile strength.', tag='p')
+    text.newline(r'$F = UTS_s A_s = \left(' + sigfigstr(boltObj.UTSs) + r'\text{ psi}\right)\left(' + sigfigstr(boltObj.As_FEDSTD_1a()) + r'\text{ in.}^{2}\right) = ' + sigfigstr(boltObj.UTSs * boltObj.As_FEDSTD_1a()) + r'\text{ lbs}$', tag='p')
+
+    # Length of Engagement
     text.newline(f'Length of Engagement', tag='h1')
     text.newline(f'The length of engagement is the measured length of interaction between a fastener and its mating material', tag='p')
     text.newline(f'The method of failure for a fastener is dependent on the length of engagement', tag='p')
