8ce50cdfde66f57089d13bc3da064e6f82ff441f
Ajonymous03 <135769503+Ajonymous03@users.noreply.github.com>
2025-07-01 13:01:28 -0400

    Fixed previous commits... and file structure

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from dictionaries.aisc import names
 from math import floor, log10, pi
 from pageText.wideFlangeText import wideFlangeText
+from pageText.FEDSTD_text import boltFEDText
 from dictionaries.ASME_B11 import ASME_B11_UN_2A2B_dict
 
 app = Flask(__name__)
@@ -43,30 +44,35 @@ def wideFlange():
     headerText, inputText, outputText, footerText = wideFlangeText(member,Fy,Eksi,Lcx,Lcy)
 
     return render_template('base.html', name = r'/wideFlange', 
-                        headerText = headerText, inputText = inputText, outputText = outputText, footerText = footerText)
+                        title = 'Wide Flange Steel Members', headerText = headerText, inputText = inputText, outputText = outputText, footerText = footerText)
     
-@app.route('/bolt', methods = ['POST', 'GET'])
+@app.route('/boltFED', methods = ['POST', 'GET'])
 def bolt():
-    
     # Gets User Inputs
-    bolt = request.form.get('boltDropdown')
-    UTSs_str = request.values.get('UTSs')
-    UTSn_str = request.values.get('UTSn')
-
-    if bolt is None and UTSs_str is None and UTSn_str is None and LE_str is None:
+    if request.form.get('boltDropdown') == None:
         bolt = '1/2-13'
-        UTSs_str = 110000
-        UTSn_str = 105000
-        LE_str = 1
+    else:
+        bolt = request.form.get('boltDropdown')
     
-    from pageText.FEDSTD_text import boltText_header, boltText_input, boltText_output, boltText_footer
-    headerText = boltText_header()
-    inputText  = boltText_input(UTSs_str, UTSn_str)
-    outputText = boltText_output(bolt, UTSs_str, UTSn_str)
-    footerText = boltText_footer()
+    if request.form.get('boltDropdown') == None:
+        bolt = '1/2-13'
+    else:
+        bolt = request.form.get('boltDropdown')
 
-    return render_template('base.html', name = r'/bolt', 
-        headerText = headerText, inputText = inputText, outputText = outputText, footerText = footerText)
+    try:
+        UTSs_str = int(request.values.get('UTSs'))
+    except:
+        UTSs_str = 110000
+    
+    try:
+        UTSn_str = int(request.values.get('UTSn'))
+    except:
+        UTSn_str = 105000
+    
+    headerText, inputText, outputText, footerText = boltFEDText(bolt, UTSs_str, UTSn_str)
+
+    return render_template('base.html', name = r'/boltFED', 
+        title = 'FED-STD H28/2B Tensile Strength', headerText = headerText, inputText = inputText, outputText = outputText, footerText = footerText)
 
 
 
