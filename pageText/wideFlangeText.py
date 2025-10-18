991c134a0a9fdad0111903e4d26cec23a3338613
Mark D. Denavit <denavit@gmail.com>
2025-10-18 10:46:10 -0400

    Modify output to use htmlstr

--- pageText/wideFlangeText.py
+++ pageText/wideFlangeText.py
@@ -64,163 +64,161 @@ def wideFlangeText(member,Fy,Eksi,Lcx,Lcy):
 
       return text.string
   
-  def output(member, A, rx, ry, bf2tf, htw, Fy, Eksi, Lcx, Lcy):
-      outputText =  f'''
-        <p>From AISC <i>Manual</i> Table 1-1, the geometric properties for a {member} are as follows:</p>
-        <p>$A_{{g}} = {{{'%.2f'%A}}}\\text{{ in.}}^{{2}}$</p> 
-        <p>$r_{{x}} = {{{'%.2f'%rx}}}\\text{{ in.}}$</p>
-        <p>$r_{{y}} = {{{'%.2f'%ry}}}\\text{{ in.}}$</p>
-        <p>$\\dfrac{{b_{{f}}}}{{2t_{{f}}}} = {{{'%.2f'%bf2tf}}}$</p>
-        <p>$\\dfrac{{h}}{{t_{{w}}}} = {{{'%.2f'%htw}}}$</p>'''
-
-      outputText += f'''
-        <h1>Slenderness Check</h1>
-        <p>The width-to-thickness ratio of the flanges of the {member} is:</p>
-        <p>$\\dfrac{{b_{{f}}}}{{2t_{{f}}}} = {{{'%.2f'%bf2tf}}}$</p>
-        <p>From AISC <i>Specification</i> Table B4.1a, Case 1, the limiting width-to-thickness ratio of the flanges is:</p>
-        <p>'''
+  def output(member, Fy, Eksi, Lcx, Lcy):
+      text = htmlstr(default_indent=10);
       
+      # Check Input
+      bad_input = False
+      
+      if member == None:
+        text.newline(f'Error: member must be defined.', tag='p')
+        bad_input = True
+
+      if Fy <= 0:
+        text.newline(f'Error: steel yield stress must be greater than zero.', tag='p')
+        bad_input = True
+
+      if Eksi <= 0:
+        text.newline(f'Error: modulus of elasticity stress must be greater than zero.', tag='p')
+        bad_input = True
+      
+      if Lcx < 0 or Lcy < 0:
+        text.newline(f'Error: effective lengths must be greater than or equal to zero.', tag='p')
+        bad_input = True
+     
+      if bad_input:
+        return text.string
+      
+      # Section Properties Data
+      A = wide_flange_database[member]['A']
+      rx = wide_flange_database[member]['rx']
+      ry = wide_flange_database[member]['ry']
+      bf2tf = wide_flange_database[member]['bf/2tf']
+      htw = wide_flange_database[member]['h/tw']
+
+      text.newline(f'From AISC <i>Manual</i> Table 1-1, the geometric properties for a {member} are as follows:', tag='p')
+      text.newline(r'$A_g = ' + sigfigstr(A) + r'\text{ in.}^2$', tag='p', cls='eqn') 
+      text.newline(r'$r_x = ' + sigfigstr(rx) + r'\text{ in.}$', tag='p', cls='eqn')
+      text.newline(r'$r_y = ' + sigfigstr(ry) + r'\text{ in.}$', tag='p', cls='eqn')
+      text.newline(r'$\dfrac{b_f}{2t_f} = ' + sigfigstr(bf2tf) + '$', tag='p', cls='eqn')
+      text.newline(r'$\dfrac{h}{t_w} = ' + sigfigstr(htw) + '$', tag='p', cls='eqn')
+
+      text.newline(r'Slenderness Check', tag='h1')
+      
+      # Check flange slenderness
       lambdar_flange = 0.56 * sqrt(Eksi / Fy)
+
+      text.newline(f'The width-to-thickness ratio of the flanges of the {member} is:', tag='p')
+      text.newline(r'$\dfrac{b_f}{2t_f} = ' + sigfigstr(bf2tf) + '$', tag='p', cls='eqn')
+      text.newline(r'From AISC <i>Specification</i> Table B4.1a, Case 1, the limiting width-to-thickness ratio of the flanges is:', tag='p')
+      
+      if bf2tf > lambdar_flange:
+          compare_result1 = r' < '
+          compare_result2 = 'slender'         
+      else:
+          compare_result1 = r' \geq '
+          compare_result2 = 'nonslender'          
+
+      text.newline(r'$\begin{aligned}\lambda_{r} &= 0.56\sqrt{\dfrac{E}{F_{y}}} \\ &= 0.56\sqrt{\dfrac{' + sigfigstr(Eksi) + r'\text{ ksi}}{' + \
+              sigfigstr(Fy) + r'\text{ ksi}}} \\ &= ' + sigfigstr(lambdar_flange) + compare_result1 + sigfigstr(bf2tf) + r'\end{aligned}$', tag='p', cls='eqn')
+      text.newline(f'therefore the flanges are {compare_result2}.', tag='p')
+      
+      # Check web slenderness
       lambdar_web = 1.49 * sqrt(Eksi / Fy)
 
-      outputText += r'$\begin{aligned}\lambda_{r} &= 0.56\sqrt{\dfrac{E}{F_{y}}} \\ &= 0.56\sqrt{\dfrac{' + sigfigstr(Eksi) + r'\text{ ksi}}{' + \
-              sigfigstr(Fy) + r'\text{ ksi}}} \\ &= ' + sigfigstr(lambdar_flange)
-
-      if bf2tf > lambdar_flange:
-          outputText += r' < ' + sigfigstr(bf2tf) + r'\end{aligned}$</p>'
-          outputText += '\n      <p>therefore the flanges are slender</p>'
-      else:
-          outputText += r' \geq ' + sigfigstr(bf2tf) + r'\end{aligned}$</p>' 
-          outputText += '\n      <p>therefore the flanges are nonslender</p>'
-  
-      outputText += f'''
-        <p>The width-to-thickness ratio of the web of the {member} is:</p>
-        <p>$\\dfrac{{h}}{{t_{{w}}}} = {{{'%.2f'%htw}}}$</p>
-        <p>From AISC <i>Specification</i> Table B4.1a, Case 5, the limiting width-to-thickness ratio of the web is:</p>
-        <p>'''
-          
-      outputText += r'$\begin{aligned}\lambda_{r} &= 1.49\sqrt{\dfrac{E}{F_{y}}} \\ &= 1.49\sqrt{\dfrac{' + sigfigstr(Eksi) + r'\text{ ksi}}{' + \
-              sigfigstr(Fy) + r'\text{ ksi}}} \\ &= ' + sigfigstr(lambdar_web)
+      text.newline(f'The width-to-thickness ratio of the web of the {member} is:', tag='p')
+      text.newline(r'$\dfrac{h}{t_w} = ' + sigfigstr(htw) + '$', tag='p', cls='eqn')
+      text.newline(r'From AISC <i>Specification</i> Table B4.1a, Case 5, the limiting width-to-thickness ratio of the web is:', tag='p')
           
       if htw > lambdar_web:
-          outputText += r' < ' + sigfigstr(htw) + r'\end{aligned}$</p>'
-          # + r'$; therefore the web is slender'      ----------- This line throws an error. Is it meant to be here?
-          outputText += '\n      <p>therefore the web is slender</p>\n'
-          
+          compare_result1 = r' < '
+          compare_result2 = 'slender'         
       else:
-          outputText += r' \geq ' + sigfigstr(htw) + r'\end{aligned}$</p>'
-          outputText += '\n      <p>therefore the web is nonslender</p>\n'
+          compare_result1 = r' \geq '
+          compare_result2 = 'nonslender'
 
-        
+      text.newline(r'$\begin{aligned}\lambda_{r} &= 1.49\sqrt{\dfrac{E}{F_{y}}} \\ &= 1.49\sqrt{\dfrac{' + sigfigstr(Eksi) + r'\text{ ksi}}{' + \
+              sigfigstr(Fy) + r'\text{ ksi}}} \\ &= ' + sigfigstr(lambdar_web) + compare_result1 + sigfigstr(htw) + r'\end{aligned}$', tag='p', cls='eqn')
+      text.newline(f'therefore the web is {compare_result2}.', tag='p')
+
+      # Check if Section E3 applies
       if bf2tf > lambdar_flange:
           if htw > lambdar_web:
-              outputText += r'      <p>Because the web and flanges are slender, the strength must be computed using AISC <i>Specification</i> Section E6 and will not be shown here.</p>'
-              return outputText
+              text.newline(r'Because the web and flanges are slender, the strength must be computed using AISC <i>Specification</i> Section E6 and will not be shown here.', tag='p')
+              return text.string
           else:
-              outputText += r'      <p>Because the flanges are slender, the strength must be computed using AISC <i>Specification</i> Section E6 and will not be shown here.</p>'
-              return outputText
+              text.newline(r'Because the flanges are slender, the strength must be computed using AISC <i>Specification</i> Section E6 and will not be shown here.', tag='p')
+              return text.string
       else:
           if htw > lambdar_web:
-              outputText += r'      <p>Because the web is slender, the strength must be computed using AISC <i>Specification</i> Section E6 and will not be shown here.</p>'
-              return outputText
+              text.newline(r'Because the web is slender, the strength must be computed using AISC <i>Specification</i> Section E6 and will not be shown here.', tag='p')
+              return text.string
           else:
-              outputText += r'      <p>Because the web and flanges are nonslender, the limit state of local buckling does not apply and the strength is computed according to AISC <i>Specification</i> Section E3.</p>'
+              text.newline(r'Because the web and flanges are nonslender, the limit state of local buckling does not apply and the strength is computed according to AISC <i>Specification</i> Section E3.', tag='p')
 
       # Gets Critical Stresses Data
-      outputText += f'''
-        <h1>Slenderness Ratio</h1>
-        <p>'''
+      text.newline(r'Slenderness Ratio', tag='h1')
 
-      outputText += r'$\dfrac{L_{cx}}{r_x} = \dfrac{' + sigfigstr(Lcx) + r'\text{ in.}}{' + \
-              sigfigstr(rx) + r'\text{ in.}} = ' + sigfigstr(Lcx/rx) + r'$'
+      text.newline(r'$\dfrac{L_{cx}}{r_x} = \dfrac{' + sigfigstr(Lcx) + r'\text{ in.}}{' + \
+              sigfigstr(rx) + r'\text{ in.}} = ' + sigfigstr(Lcx/rx) + r'$', tag='p', cls='eqn')
 
-      outputText += f'''</p>
-        <p>'''
 
-      outputText += r'$\dfrac{L_{cy}}{r_y} = \dfrac{' + sigfigstr(Lcy) + r'\text{ in.}}{' + \
-              sigfigstr(ry) + r'\text{ in.}} = ' + sigfigstr(Lcy/ry) + r'$'
+      text.newline(r'$\dfrac{L_{cy}}{r_y} = \dfrac{' + sigfigstr(Lcy) + r'\text{ in.}}{' + \
+              sigfigstr(ry) + r'\text{ in.}} = ' + sigfigstr(Lcy/ry) + r'$', tag='p', cls='eqn')
 
-      outputText += f'''</p>
-        <p>'''
         
       if Lcx/rx > Lcy/ry:
-          outputText += r'Because $\dfrac{L_{cx}}{r_x} > \dfrac{L_{cy}}{r_y}$, major-axis buckling controls and $\dfrac{L_{c}}{r} =  ' + sigfigstr(Lcx/rx) + '$'
+          text.newline(r'Because $\dfrac{L_{cx}}{r_x} > \dfrac{L_{cy}}{r_y}$, major-axis buckling controls and $\dfrac{L_{c}}{r} =  ' + sigfigstr(Lcx/rx) + '$', tag='p')
           Lc_over_r = Lcx/rx
       else:
-          outputText += r'Because $\dfrac{L_{cy}}{r_y} > \dfrac{L_{cx}}{r_x}$, minor-axis buckling controls and $\dfrac{L_{c}}{r} =  ' + sigfigstr(Lcy/ry) + '$'
+          text.newline(r'Because $\dfrac{L_{cy}}{r_y} > \dfrac{L_{cx}}{r_x}$, minor-axis buckling controls and $\dfrac{L_{c}}{r} =  ' + sigfigstr(Lcy/ry) + '$', tag='p')
           Lc_over_r = Lcy/ry
 
       
-      outputText += f'''
-        <h1>Nominal Compressive Strength</h1>
+      text.newline(r'Nominal Compressive Strength', tag='h1')
 
-        <p>The available critical stresses may be interpolated from AISC <i>Manual</i> Table 4-14 or calculated directly according to AISC <i>Specification</i> Section E3 as follows.</p>
-        <p>Calculate the elastic critical buckling stress, $F_{{e}}$</p>
-        <p>'''
+      text.newline(r'The available critical stresses may be interpolated from AISC <i>Manual</i> Table 4-14 or calculated directly according to AISC <i>Specification</i> Section E3 as follows.', tag='p')
+      text.newline(r'Calculate the elastic critical buckling stress, $F_{{e}}$', tag='p')
 
       Fe = pi**2 * Eksi / Lc_over_r**2
-      outputText += r'$\begin{aligned}F_e &= \dfrac{\pi^2 E}{\left(\dfrac{L_c}{r}\right)^2} \\' + \
+      text.newline(r'$\begin{aligned}F_e &= \dfrac{\pi^2 E}{\left(\dfrac{L_c}{r}\right)^2} \\' + \
               r'&= \dfrac{\pi^2 (' + sigfigstr(Eksi) + r'\text{ ksi})}{(' + sigfigstr(Lc_over_r) + r')^2} \\' + \
-              r'&= ' + sigfigstr(Fe) + r'\text{ ksi} \end{aligned}$</p>' 
+              r'&= ' + sigfigstr(Fe) + r'\text{ ksi} \end{aligned}$', tag='p', cls='eqn')
 
-      outputText += f'''
-        <p>Calculate the nominal stress, $F_{{n}}$</p>
-        <p>'''
+      text.newline('Calculate the nominal stress, $F_{{n}}$', tag='p')
 
-      outputText += r'$4.71\sqrt{\dfrac{E}{F_y}} = 4.71\sqrt{\dfrac{' + sigfigstr(Eksi) + r'\text{ ksi}}{' + sigfigstr(Fy) + r'\text{ ksi}}} = ' + sigfigstr(4.71*sqrt(Eksi/Fy)) + r'$'
-      
-      outputText += f'''</p>
-        <p>'''
-      
+      text.newline(r'$4.71\sqrt{\dfrac{E}{F_y}} = 4.71\sqrt{\dfrac{' + sigfigstr(Eksi) + r'\text{ ksi}}{' + sigfigstr(Fy) + r'\text{ ksi}}} = ' + sigfigstr(4.71*sqrt(Eksi/Fy)) + r'$', tag='p', cls='eqn')
+           
       if Lc_over_r <= 4.71*sqrt(Eksi/Fy):
-          outputText += r'Because $\dfrac{L_c}{r} = ' + sigfigstr(Lc_over_r) + r'\leq 4.71\sqrt{\dfrac{E}{F_y}} = ' + sigfigstr(4.71*sqrt(Eksi/Fy)) + r'$, inelastic buckling governs and $F_n$ is determined using AISC <i>Specification</i> Equation E3-2'
-
-          outputText += f'''</p>
-        <p>'''
-
+          text.newline(r'Because $\dfrac{L_c}{r} = ' + sigfigstr(Lc_over_r) + r'\leq 4.71\sqrt{\dfrac{E}{F_y}} = ' + sigfigstr(4.71*sqrt(Eksi/Fy)) + r'$, inelastic buckling governs and $F_n$ is determined using AISC <i>Specification</i> Equation E3-2', tag='p')
 
           Fn = 0.658**(Fy/Fe)*Fy
 
-          outputText += r'$\begin{aligned}F_n &= \left(0.658^{\dfrac{F_y}{F_e}}\right)F_y \\' + \
+          text.newline(r'$\begin{aligned}F_n &= \left(0.658^{\dfrac{F_y}{F_e}}\right)F_y \\' + \
                   r'&= \left[0.658^\dfrac{' + sigfigstr(Fy) + r'\text{ ksi}}{' + sigfigstr(Fe) + r'\text{ ksi}}\right](' + sigfigstr(Fy) + r'\text{ ksi}) \\' + \
-                  r'&=' + sigfigstr(Fn) + r'\text{ ksi} \end{aligned}$</p>'
+                  r'&=' + sigfigstr(Fn) + r'\text{ ksi} \end{aligned}$', tag='p', cls='eqn')
                   
       else:
-          outputText += r'Because $\dfrac{L_c}{r} = ' + sigfigstr(Lc_over_r) + r'> 4.71\sqrt{\dfrac{E}{F_y}} = ' + sigfigstr(4.71*sqrt(Eksi/Fy)) + r'$, elastic buckling governs and $F_n$ is determined using AISC <i>Specification</i> Equation E3-3'
+          text.newline(r'Because $\dfrac{L_c}{r} = ' + sigfigstr(Lc_over_r) + r'> 4.71\sqrt{\dfrac{E}{F_y}} = ' + sigfigstr(4.71*sqrt(Eksi/Fy)) + r'$, elastic buckling governs and $F_n$ is determined using AISC <i>Specification</i> Equation E3-3', tag='p')
       
-          outputText += f'''</p>
-        <p>'''
-
           Fn = 0.877 * Fe
 
-          outputText += r'$\begin{aligned}F_n &= 0.877 F_e \\' + \
+          text.newline(r'$\begin{aligned}F_n &= 0.877 F_e \\' + \
                   r'&= 0.877 (' + sigfigstr(Fe) + r'\text{ ksi}) \\' + \
-                  r'&=' + sigfigstr(Fn) + r'\text{ ksi} \end{aligned}$</p>'
+                  r'&=' + sigfigstr(Fn) + r'\text{ ksi} \end{aligned}$', tag='p', cls='eqn')
 
 
-      outputText += f'''
-        <p>Calculate the nominal axial compressive strength, $P_{{n}}$, using  AISC <i>Specification</i> Equation E3-1.</p>
-        <p>'''
+      text.newline('Calculate the nominal axial compressive strength, $P_{{n}}$, using  AISC <i>Specification</i> Equation E3-1.', tag='p')
+
       Pn = Fn * A
 
-      outputText += r'$P_n = F_n A_g = (' + sigfigstr(Fn) + r'\text{ ksi})(' + sigfigstr(A) + r'\text{ in.}^2) = ' + sigfigstr(Pn) + r'\text{ kips} $</p>'
-
-      phiPn = 0.9 * Pn
-      omegaPn = Pn / 1.67 
+      text.newline(r'$P_n = F_n A_g = (' + sigfigstr(Fn) + r'\text{ ksi})(' + sigfigstr(A) + r'\text{ in.}^2) = ' + sigfigstr(Pn) + r'\text{ kips}$', tag='p', cls='eqn') 
+      text.newline('For LRFD:', tag='p')
+      text.newline(r'$\phi_c P_n = 0.90 (' + sigfigstr(Pn) + r'\text{ kips}) = ' + sigfigstr(0.9*Pn) + r'\text{ kips}$', tag='p', cls='eqn')     
+      text.newline('For ASD:', tag='p')
+      text.newline(r'$\dfrac{P_n}{\Omega_c} = \dfrac{(' + sigfigstr(Pn) + r'\text{ kips})}{1.67} = ' + sigfigstr(Pn/1.67) + r'\text{ kips} $', tag='p', cls='eqn')     
       
-      outputText += f'''
-        <p>For LRFD:</p>
-        <p>'''
-
-      outputText += r'$\phi_c P_n = 0.90 (' + sigfigstr(Pn) + r'\text{ kips}) = ' + sigfigstr(0.9*Pn) + r'\text{ kips} $</p>'
-
-      outputText += f'''
-        <p>For ASD:</p>
-        <p>'''
-
-      outputText += r'$\dfrac{P_n}{\Omega_c} = \dfrac{(' + sigfigstr(Pn) + r'\text{ kips})}{1.67} = ' + sigfigstr(Pn/1.67) + r'\text{ kips} $</p>'
-      
-      return outputText
+      return text.string
   
   def footer():
       text = htmlstr(default_indent=10);
@@ -228,21 +226,4 @@ def wideFlangeText(member,Fy,Eksi,Lcx,Lcy):
       text.newline('This work was supported by the Naval Engineering Education Consortium [Award Number N00174-22-1-0017].', tag='p')
       return text.string
 
-  # Check output
-  if member == None:
-      return header(), input(Fy, Eksi, Lcx, Lcy), '', footer()
-  elif Fy <= 0:
-      return header(), input(Fy, Eksi, Lcx, Lcy), 'Steel yield stress must be greater than zero', footer()       
-  elif Eksi <= 0:
-      return header(), input(Fy, Eksi, Lcx, Lcy), 'Modulus of elasticity must be greater than zero', footer()
-  elif Lcx < 0 or Lcy < 0:
-      return header(), input(Fy, Eksi, Lcx, Lcy), 'The effective lengths must be greater than or equal to zero', footer()
-  else:
-    # Section Properties Data
-    A = wide_flange_database[member]['A']
-    rx = wide_flange_database[member]['rx']
-    ry = wide_flange_database[member]['ry']
-    bf2tf = wide_flange_database[member]['bf/2tf']
-    htw = wide_flange_database[member]['h/tw']
-
-    return header(), input(Fy, Eksi, Lcx, Lcy), output(member, A, rx, ry, bf2tf, htw, Fy, Eksi, Lcx, Lcy), footer()
+  return header(), input(Fy, Eksi, Lcx, Lcy), output(member, Fy, Eksi, Lcx, Lcy), footer()
